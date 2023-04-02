66142ed6f6b0a9cdc28cd0f81781e598db1a55b0
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-04-02 18:10:20 +0200

    Update README.md

--- Puzzles/Easy/Binary image/Javascript.js
+++ Puzzles/Easy/Binary image/Javascript.js
@@ -1,31 +1,28 @@
-const draw = []
+const draw = [];
 const h = parseInt(readline());
-let valid = true
-let length = 0
+let valid = true;
+let length = 0;
 for (let i = 0; i < h; i++) {
-    const row = readline().split(' ');
-    let str = ""
-    for(let j = 0; j < row.length; j++){
-        if(j % 2 == 0){
-            str += ".".repeat(row[j])
-        }
-        else{
-            str += "O".repeat(row[j])
-        }
+  const row = readline().split(" ");
+  let str = "";
+  for (let j = 0; j < row.length; j++) {
+    if (j % 2 == 0) {
+      str += ".".repeat(row[j]);
+    } else {
+      str += "O".repeat(row[j]);
     }
-    if(i == 0){
-        length = str.length
-    }
-    else if(str.length != length){
-        valid = false
-    }
-    draw.push(str)
+  }
+  if (i == 0) {
+    length = str.length;
+  } else if (str.length != length) {
+    valid = false;
+  }
+  draw.push(str);
 }
-if(valid){
-    for (let i = 0; i < h; i++) {
-        console.log(draw[i]);
-    }
-}
-else{
-    console.log('INVALID')
+if (valid) {
+  for (let i = 0; i < h; i++) {
+    console.log(draw[i]);
+  }
+} else {
+  console.log("INVALID");
 }

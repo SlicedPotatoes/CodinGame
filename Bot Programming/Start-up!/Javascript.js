5f789f950d8211429a44184396710c6c0f61e18e
SlicedPotatoes <53843746+SlicedPotatoes@users.noreply.github.com>
2023-09-25 23:46:14 +0200

    Update Bot Programming Start-up!

--- Bot Programming/Start-up!/Javascript.js
+++ Bot Programming/Start-up!/Javascript.js
@@ -1,3 +1,27 @@
+class Player {
+  constructor(id, income, cash, devs, sellers, managers, features, tests, bugs) {
+    this.id = id;
+    this.income = income;
+    this.cash = cash;
+    this.devs = devs;
+    this.sellers = sellers;
+    this.managers = managers;
+    this.features = features;
+    this.tests = tests;
+    this.bugs = bugs;
+  }
+  debug() {
+    console.error(`ID: ${this.id}\nINCOME: ${this.income}\nCASH: ${this.cash}\nDEVS: ${this.devs}\nSELLERS: ${this.sellers}\nMANAGERS: ${this.managers}\nFEATURES: ${this.features}\nTESTS: ${this.tests}\nBUGS: ${this.bugs}`);
+  }
+}
+class Concurent {
+  constructor(id, marketShare, reputation) {
+    this.id = id;
+    this.marketShare = marketShare;
+    this.reputation = reputation;
+  }
+}
+const POIDS = [0.5];
 while (true) {
   const id = parseInt(readline()); // Your player id
   const playerCount = parseInt(readline()); // Number of players
@@ -10,58 +34,64 @@ while (true) {
   const features = parseInt(readline());
   const tests = parseInt(readline()); // Tests developed in your software
   const bugs = parseInt(readline()); // Bugs in your software
-
-  console.error(`income ${income}`);
-  console.error(`cash ${cash}`);
-  console.error(`devs ${devs}`);
-  console.error(`sellers ${sellers}`);
-  console.error(`managers ${managers}`);
-  console.error(`features ${features}`);
-  console.error(`tests ${tests}`);
-  console.error(`bugs ${bugs}`);
-
+  const player = new Player(id, income, cash, devs, sellers, managers, features, tests, bugs);
+  player.debug();
+  const concurents = [];
   for (let i = 0; i < playerCount; i++) {
     var inputs = readline().split(" ");
     const startUpId = parseInt(inputs[0]); // Start-up id
     const marketShare = parseInt(inputs[1]); // Market share of the start-up in thousands
     const reputation = parseInt(inputs[2]); // Reputation of the start-up
-    console.error(`startUpId ${startUpId}`);
-    console.error(`marketShare ${marketShare}`);
-    console.error(`reputation ${reputation}`);
+    concurents.push(new Concurent(startUpId, marketShare, reputation));
   }
-  let devToHire = GetdevToHire(managers, devs, sellers, features);
-  let sellersToHire = GetsellersToHire(managers, devs, sellers, features);
-  let managersToHire = getManagersToHire(managers, devs, sellers);
-  let maintenanceDevs = (devToHire + devs) / 2;
-  let competitiveSellers = 0;
-  let targetId = 0;
-  console.log(`${devToHire} ${sellersToHire} ${managersToHire} ${maintenanceDevs} ${competitiveSellers}`);
+  // <devsToHire> <sellerToHire> <managersToHire> <maintenanceDevs> <competitiveSellers> <targetId>
+  console.log(f(player, concurents));
 }
-function GetdevToHire(managers, devs, sellers, features) {
-  if (!needSellers(features, sellers)) {
-    let dTH = 4 * managers - (devs + sellers);
-    dTH = dTH > 2 * managers ? 2 * managers : dTH;
-    return dTH;
+function f(player, concurents) {
+  let devsToHire = 0;
+  let sellerToHire = 0;
+  let managersToHire = 0;
+  let maintenanceDevs = 0;
+  let competitivesSellers = 0;
+  let targetId = -1;
+  function getNbSalarie() {
+    return player.devs + player.sellers;
   }
-  return 0;
-}
-function GetsellersToHire(managers, devs, sellers, features) {
-  if (needSellers(features, sellers)) {
-    let sTH = 4 * managers - (devs + sellers);
-    sTH = sTH > 2 * managers ? 2 * managers : sTH;
-    return sTH;
+  function getMoneyNeeded() {
+    let money = 0;
+    money += player.managers * 20;
+    money += getNbSalarie() * 10;
+    return money;
   }
-  return 0;
-}
-function getManagersToHire(managers, devs, sellers) {
-  if (devs + sellers == 4 * managers) {
-    return 1;
+  function getNeedSalarie() {
+    let n = player.managers * 4 - player.devs;
+    return n > player.managers * 2 ? player.managers * 2 : n;
   }
-  return 0;
-}
-function needSellers(features, sellers) {
-  if (features >= 10 && sellers < 1) {
-    return true;
+  function needManager() {
+    return getNbSalarie() / 4 == player.managers;
   }
-  return false;
+  let moneyNeededThisTurn = getMoneyNeeded();
+  let needSalarie = getNeedSalarie();
+  while (needSalarie > 0 && player.cash - moneyNeededThisTurn >= 10) {
+    if (player.features < 10) {
+      devsToHire++;
+    } else {
+      if (player.devs + devsToHire / getNbSalarie() + devsToHire + sellerToHire < POIDS[0]) {
+        devsToHire++;
+      } else {
+        sellerToHire++;
+      }
+    }
+
+    needSalarie--;
+    moneyNeededThisTurn += 10;
+  }
+  if (needManager() && player.cash - moneyNeededThisTurn >= 20) {
+    managersToHire = 1;
+    moneyNeededThisTurn += 20;
+  }
+  let totalDev = player.devs + devsToHire;
+  maintenanceDevs = totalDev - Math.round((totalDev * 20) / 100);
+
+  return `${devsToHire} ${sellerToHire} ${managersToHire} ${maintenanceDevs} ${competitivesSellers} ${targetId == -1 ? "" : targetId}`;
 }
